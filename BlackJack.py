b9f57689a1f4c7a42ef324c12ec9e0e84ed6abb6
sharadpatel11 <54919810+sharadpatel11@users.noreply.github.com>
2023-09-17 20:28:30 -0400

    File Updated

--- BlackJack.py
+++ BlackJack.py
@@ -6,28 +6,67 @@ cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 cardDeck = {"a": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11}
 suits = ['♠', '♥', '♣', '♦']
 
-os.system('cls' if os.name == 'nt' else 'clear')
-print(BlackJack_Art.logo)
+replay = "yes"
+while replay == "yes":
+    replay = "none"
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(BlackJack_Art.logo)
+    userCards = [random.choice(cards), random.choice(cards)]
+    userCardSum = cardDeck[userCards[0]] + cardDeck[userCards[1]]
+    print(f"\nYour Cards: [{userCards[0]}{random.choice(suits)}, {userCards[1]}{random.choice(suits)}]. Total: {userCardSum}")
 
-userCards = [random.choice(cards), random.choice(cards)]
-userCardSum = cardDeck[userCards[0]] + cardDeck[userCards[1]]
-print(f"\nYour Cards: [{userCards[0]}{random.choice(suits)}, {userCards[1]}{random.choice(suits)}]. Total: {userCardSum}")
+    dealerCards = [random.choice(cards), random.choice(cards)]
+    dealerCardSum = cardDeck[dealerCards[0]] + cardDeck[dealerCards[1]]
+    print(f"\nDealer has [{dealerCards[0]}{random.choice(suits)}]. Total: {cardDeck[dealerCards[0]]}")
 
-dealerCards = [random.choice(cards), random.choice(cards)]
-dealerCardSum = cardDeck[dealerCards[0]] + cardDeck[userCards[1]]
-print(f"Dealer cards: [{dealerCards[0]}{random.choice(suits)}]. Total: {cardDeck[dealerCards[0]]}")
+    userChoice = input("\nWould you like to Hit or Stay?: ")
+    userChoice = userChoice.lower()
+    while userChoice == "hit" and userCardSum <= 21:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(BlackJack_Art.logo)
+        userCards.append(random.choice(cards))
+        userCardSum += cardDeck[userCards[len(userCards) - 1]]
+        print(f"\nYour Cards: {userCards}. Total: {userCardSum}")
+        if userCardSum > 21:
+            print("Sorry BUST. You lose")
+        else:
+            userChoice = input("\nWould you like to Hit or Stay?: ")
+            userChoice = userChoice.lower()
+        print(f"\nDealer has [{dealerCards[0]}{random.choice(suits)}]. Total: {cardDeck[dealerCards[0]]}")
 
-userChoice = input("\nWould you like to Hit or Stay?: ")
-userChoice = userChoice.lower()
-while userChoice == "hit" and userCardSum <= 21:
-    userCards.append(random.choice(cards))
-    userCardSum += cardDeck[userCards[len(userCards) - 1]]
-    print(f"\nYour Cards: {userCards}. Total: {userCardSum}")
-    if userCardSum > 21:
-        print("Sorry you BUST.")
+
+    if(userCardSum <= 21):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(BlackJack_Art.logo)
+        print(f"\nYour Cards: {userCards}. Total: {userCardSum}")
+        while dealerCardSum < userCardSum:
+            dealerCards.append(random.choice(cards))
+            dealerCardSum += cardDeck[dealerCards[len(dealerCards) - 1]]
+        
+        print("\n")
+        if dealerCardSum > 21:
+            print(f"Dealer has {dealerCards}. Total: {dealerCardSum}")
+            print("Dealer BUST! You WIN")
+        elif dealerCardSum == userCardSum:
+            print(f"Dealer has {dealerCards}. Total: {dealerCardSum}")
+            print("Tie")
+        else:
+            print(f"Dealer has {dealerCards}. Total: {dealerCardSum}")
+            print("You lose.")
+
+        replay = input("\nWould you like to continue playing? Yes or No: ")
+        replay.lower()
     else:
-        userChoice = input("\nWould you like to Hit or Stay?: ")
-        userChoice = userChoice.lower()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(BlackJack_Art.logo)
+        print(f"\nYour Cards: {userCards}. Total: {userCardSum}")
+        while dealerCardSum < 21:
+            dealerCards.append(random.choice(cards))
+            dealerCardSum += cardDeck[dealerCards[len(dealerCards) - 1]]
+        
+        print("\n")
+        print(f"Dealer has {dealerCards}. Total: {dealerCardSum}")
+        replay = input("\nWould you like to continue playing? Yes or No: ")
+        replay.lower()
 
-if(userCardSum <= 21):
-    print(f"\nYour Cards: {userCards}. Total: {userCardSum}")
+os.system('cls' if os.name == 'nt' else 'clear')
